52dabd584ba3910eb653947ae9a9ea4878fa86f9
Satyam Patil <patilsatyam36@gmail.com>
2023-08-24 11:06:50 +0530

    signout

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -19,6 +19,7 @@ export default function AuthProvider({ children }) {
       if (user) {
         setuser(user);
         setisLoggedin(true);
+        navigate("/");
       } else {
         setuser(null);
       }
